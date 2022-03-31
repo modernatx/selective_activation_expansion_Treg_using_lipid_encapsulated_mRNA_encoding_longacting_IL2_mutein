51c6c978e120fe1f00f00671a49c43e9b47504cb
Chiao-wen Hsiao <joyce.hsiao1@gmail.com>
2022-03-31 18:25:04 +0000

    add Figure S7B

---
+++ notebooks_scripts/FigureS7_cellcycle.R
@@ -0,0 +1,52 @@
+#################################################################################
+# 
+# Cell cycle stage assignment
+#
+# Software: scran::cyclone v1.20.1 
+# Outcome: Assign each single cell to a discrete cell cycle stage (G1, S, G2.M).
+#################################################################################
+
+library(scran)
+
+# import mouse cycle cycle marker genes
+mm.pairs <- readRDS(system.file("exdata", "mouse_cycle_markers.rds", 
+package="scran"))
+
+# import SCE object
+sce = readRDS(file = "./data/sce.rds")
+    
+# get EMSEMBL gene ID
+library(biomaRt)
+ensembl_mus <- useEnsembl(biomart = "ensembl", dataset = "mmusculus_gene_ensembl")
+biomart_res <- getLDS(attributes="external_gene_name", 
+                      filters = "external_gene_name", 
+                      values = rownames(sce), mart=ensembl_mus, 
+                      attributesL="ensembl_gene_id", martL=ensembl_mus)
+
+    
+# Using Ensembl IDs to match up with the annotation in 'mm.pairs'.
+rowData(sce) = data.frame(Symbol = rownames(sce), 
+        Ensembl = biomart_res$Gene.stable.ID[match(rownames(sce), biomart_res$Gene.name)])
+assignments <- cyclone(sce, 
+                       mm.pairs, gene.names=rowData(sce)$Ensembl)
+    
+    
+assignments <- readRDS("/root/IL2_timecourse/output/Cyclone/assignments.rds")    
+    
+    
+
+
+# Figure S7A
+tab =  as.data.frame(table(assignments$phases, clust_assign_k_18))
+ggplot(tab, aes(x = clust_assign_k_18, y = Freq, fill = Var1)) + 
+  geom_bar(position="fill", stat="identity") +
+  labs(title = "Cyclone predicted Cell cycle phase\n", x = "\nTopics", y = "Frequency\n") +
+  theme_classic()
+
+# Figure S7B
+tab =  as.data.frame(table(assignments$phases, clust_assign_k_18))
+ggplot(tab, aes(x = clust_assign_k_18, y = Freq, fill = Var1)) + 
+  geom_bar(position="fill", stat="identity") +
+  labs(title = "Cyclone predicted Cell cycle phase\n", x = "\nTopics", y = "Frequency\n") +
+  theme_classic()
+    
